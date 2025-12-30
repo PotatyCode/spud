fc7d24bd61a0fbae9197aa1ffb75c04845d354b3
PotatyCode <Potatyyman@proton.me>
2025-12-30 11:31:44 +1000

    added run.rs

---
+++ src/run.rs
@@ -0,0 +1,20 @@
+use anyhow::{Context, Ok, Result};
+use std::process::Command;
+
+use crate::config::Config;
+pub fn run(config: &Config, args: Vec<String>) -> Result<()> {
+    Command::new(format!("./build/{}", config.project.name))
+        .args(args)
+        .stdout(std::process::Stdio::inherit())
+        .stderr(std::process::Stdio::inherit())
+        .status()
+        .context("cannot run executable build/")?;
+    Ok(())
+}
+pub fn split_args(args: Vec<String>) -> (Vec<String>, Vec<String>) {
+    let split_pos = args.iter().position(|arg| arg == "::");
+    match split_pos {
+        Some(pos) => (args[..pos].to_vec(), args[pos + 1..].to_vec()),
+        None => (args, vec![]),
+    }
+}
