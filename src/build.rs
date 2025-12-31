8b88169716fe6be7db421475d682ff3f43747a0e
PotatyCode <Potatyyman@proton.me>
2025-12-31 10:51:35 +1000

    added clang as default compiler and ninja as generator

--- src/build.rs
+++ src/build.rs
@@ -2,16 +2,31 @@ use std::process::Command;
 
 use crate::config::Config;
 use anyhow::{Context, Result};
-pub fn build(_config: &Config) -> Result<()> {
-    let cmake_args = [".", "-B", "build"];
-    Command::new("cmake")
+pub fn build(config: &Config) -> Result<()> {
+    let cmake_args = [
+        ".",
+        "-B",
+        "build",
+        "-G",
+        "Ninja",
+        &format!("-DCMAKE_CXX_COMPILER={}", config.project.compiler),
+    ];
+    let output = Command::new("cmake")
         .args(cmake_args)
         .output()
         .context("Cmake failed to build")?;
 
-    Command::new("cmake")
+    println!("Status: {}", output.status);
+    println!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+
+    let output = Command::new("cmake")
         .args(["--build", "build"])
         .output()
         .context("CMake build failed")?;
+    // SHOW EVERYTHING
+    println!("=== CMAKE BUILD ===");
+    println!("{}", String::from_utf8_lossy(&output.stdout));
+    println!("{}", String::from_utf8_lossy(&output.stderr));
     Ok(())
 }
--- src/init/scripts/cmake.rs
+++ src/init/scripts/cmake.rs
@@ -5,18 +5,7 @@ use crate::config::Config;
 pub fn cmake_template(config: &Config) -> String {
     formatdoc! {r#"
 cmake_minimum_required (VERSION 4.2.1)
-project({project_name} CXX) 
-
-set(CMAKE_CXX_STANDARD, {cpp_standard})
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
+project({project_name}) 
 add_executable({project_name} src/main.cpp)
-
-target_compile_options({project_name} PRIVATE
-  -Wall -Wextra -Wpedantic
-  $<$<CONFIG:Debug>:-g>
-  $<$<CONFIG:Release>:-03>
-)
-    "#, project_name = config.project.name,
-    cpp_standard = config.project.cpp_standard}
+    "#, project_name = config.project.name}
 }
