6f2e5f9f8562e5e6c0799d52e43e430dd8dadc9d
PotatyCode <Potatyyman@proton.me>
2025-12-31 10:26:09 +1000

    updated cmake config with cpp standard and -W flags

--- src/init/scripts/cmake.rs
+++ src/init/scripts/cmake.rs
@@ -5,7 +5,18 @@ use crate::config::Config;
 pub fn cmake_template(config: &Config) -> String {
     formatdoc! {r#"
 cmake_minimum_required (VERSION 4.2.1)
-project({project_name}) 
+project({project_name} CXX) 
+
+set(CMAKE_CXX_STANDARD, {cpp_standard})
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
 add_executable({project_name} src/main.cpp)
-    "#, project_name = config.project.name}
+
+target_compile_options({project_name} PRIVATE
+  -Wall -Wextra -Wpedantic
+  $<$<CONFIG:Debug>:-g>
+  $<$<CONFIG:Release>:-03>
+)
+    "#, project_name = config.project.name,
+    cpp_standard = config.project.cpp_standard}
 }
